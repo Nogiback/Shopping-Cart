06a3a54e7e4c50729df66a0330e7f07b727f0eec
Nogiback <peter.toan.do@gmail.com>
2023-12-10 15:56:18 -0500

    Setting up tailwind + prettier auto class sorting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div className='flex justify-between items-center p-4 bg-sky-700'>
-      <h1 className= 'text-orange-500 text-3xl font-bold'>Nogi Shop</h1>
-      <div className='flex gap-4'>
-        <Link to='/'>Home</Link>
-        <Link to='/store'>Shop</Link>
-        <Link to='/cart'>Cart</Link>
+    <div className="flex items-center justify-between bg-sky-700 p-4">
+      <h1 className="text-3xl font-bold text-orange-500">Nogi Shop</h1>
+      <div className="flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/store">Shop</Link>
+        <Link to="/cart">Cart</Link>
       </div>
     </div>
-  )
+  );
 }

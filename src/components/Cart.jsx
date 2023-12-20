003b5e7efc7c8bd2ac2124d6edc50240803cd0db
Nogiback <peter.toan.do@gmail.com>
2023-12-20 15:31:58 -0500

    Refactored cart component, create cartItem component to clean up code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import { X, MinusCircle, PlusCircle, Trash } from 'lucide-react';
+import { X } from 'lucide-react';
+import CartItem from './CartItem';
 
 export default function Cart({
   cartStatus,
@@ -57,47 +58,13 @@ export default function Cart({
             <div className='flex w-full flex-col gap-4 overflow-y-scroll'>
               {cart.map((item) => {
                 return (
-                  <div key={item.id} className='flex w-full gap-2 pb-4'>
-                    <img
-                      src={item.img}
-                      alt={item.name}
-                      className='h-32 w-32 rounded-lg'
-                    ></img>
-                    <div className='flex w-full flex-col justify-between'>
-                      <div>
-                        <h3 className='text-lg'>{item.name}</h3>
-                        <h4 className=''>${item.price}</h4>
-                      </div>
-                      <div className='flex w-full justify-between'>
-                        <div className='flex'>
-                          <button>
-                            <MinusCircle
-                              type='button'
-                              className='hover:text-sage-500 h-6 w-6'
-                              onClick={() => onDecrease(item)}
-                            />
-                          </button>
-                          <div className='text-forest-900 w-8 text-center text-lg font-semibold'>
-                            {item.quantity}
-                          </div>
-                          <button>
-                            <PlusCircle
-                              type='button'
-                              className='hover:text-sage-500 h-6 w-6'
-                              onClick={() => onIncrease(item)}
-                            />
-                          </button>
-                        </div>
-                        <button>
-                          <Trash
-                            type='button'
-                            className='h-6 w-6 pr-2 transition-colors hover:text-[red]'
-                            onClick={() => deleteItem(item)}
-                          />
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  <CartItem
+                    key={item.id}
+                    item={item}
+                    onDecrease={onDecrease}
+                    onIncrease={onIncrease}
+                    deleteItem={deleteItem}
+                  />
                 );
               })}
             </div>
